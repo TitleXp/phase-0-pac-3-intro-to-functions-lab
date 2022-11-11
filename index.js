88b21e3208af4e0488c52665a7646f8947041283
Surapat Mekvanich <titleyugio@gmail.com>
2022-11-10 23:11:07 -0800

    commit tree 64f300a521e1042074f2ee086aaf695c4e5eed4b

---
+++ index.js
@@ -0,0 +1,11 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+function sayHiToHeadphonedRoomate() {
+    return console.log("I can't hear you")
+}
